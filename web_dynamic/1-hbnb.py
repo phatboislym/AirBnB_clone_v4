8710a4dec23ee4d82bf0dc51735cd3978886be83
Christian Koranteng <korantengchristian@gmail.com>
2023-01-28 17:45:52 +0000

    Upload new changes

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb', strict_slashes=False)
+@app.route('/1-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -34,8 +34,10 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
+
     cache_id = uuid.uuid4()
-    return render_template('0-hbnb.html',
+
+    return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
@@ -44,10 +46,4 @@ def hbnb():
 
 if __name__ == "__main__":
     """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port)
+    app.run(host='0.0.0.0', port='5000')
--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb', strict_slashes=False)
+@app.route('/2-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -34,8 +34,10 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
+
     cache_id = uuid.uuid4()
-    return render_template('0-hbnb.html',
+
+    return render_template('2-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
